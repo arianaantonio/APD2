37a855011c10b4a1540c0d244a2c2753e965d3a8
p32424 <faeryqueen21@fullsail.edu>
2014-09-10 00:06:11 -0700

    Added splash screen

--- AstroPix/gen/com/arianaantonio/astropix/R.java
+++ AstroPix/gen/com/arianaantonio/astropix/R.java
@@ -10,6 +10,9 @@ package com.arianaantonio.astropix;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Black=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -29,25 +32,33 @@ public final class R {
     }
     public static final class drawable {
         public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_action_about=0x7f020001;
+        public static final int ic_action_favorite=0x7f020002;
+        public static final int ic_action_go_to_today=0x7f020003;
+        public static final int ic_action_picture=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_drawer=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int splash=0x7f020008;
     }
     public static final class id {
-        public static final int action_example=0x7f080005;
-        public static final int action_settings=0x7f080004;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080003;
+        public static final int action_example=0x7f090006;
+        public static final int action_settings=0x7f090005;
+        public static final int container=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int imageView1=0x7f090004;
+        public static final int navigation_drawer=0x7f090002;
+        public static final int section_label=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
         public static final int fragment_navigation_drawer=0x7f030002;
+        public static final int splash_screen=0x7f030003;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int global=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         public static final int action_example=0x7f050006;
@@ -55,6 +66,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int navigation_drawer_close=0x7f050005;
         public static final int navigation_drawer_open=0x7f050004;
+        public static final int splash=0x7f050008;
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
@@ -81,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- AstroPix/src/com/arianaantonio/astropix/MainActivity.java
+++ AstroPix/src/com/arianaantonio/astropix/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private NavigationDrawerFragment mNavigationDrawerFragment;   
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
